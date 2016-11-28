8896423688799545f9712edc42b317a084139a48
x10an14 <x10an14@hotmail.com>
2016-11-28 16:46:22 +0100

    Initial manual (non-github) commit.

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
-# overtime-calculator
+# Overtime-Calculator
 Personal attempt at simplifying the calculation of what I've accumulated (potentially deficit) over-time!
---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name='Overtime-Calculator',
+    version='0.1',
+    # description='Python Distribution Utilities',
+    # author='Greg Ward',
+    # author_email='gward@python.net',
+    # url='https://www.python.org/sigs/distutils-sig/',
+    packages=['webapp'],)
