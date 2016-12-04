5c39fea3e3bb1250a1d79e6bce51246f4e1ffcb2
x10an14 <x10an14@hotmail.com>
2016-12-04 20:07:18 +0100

    Feature: Now all functions in src/calculations.py have simple verification tests!

--- src/calculations.py
+++ src/calculations.py
@@ -26,7 +26,7 @@ def parse_row(row, field_names, datetime_parse_fmt=default_parse_fmt, has_durati
         stop_time = datetime.strptime(duration, datetime_parse_fmt)
 
         # Get duration as "HH:MM":
-        minutes = (stop_time - start_time).seconds // 60
+        minutes = int((stop_time - start_time).total_seconds() // 60)
         hours = str(minutes // 60).zfill(2)
         minutes = str(minutes % 60).zfill(2)
 
@@ -36,62 +36,64 @@ def parse_row(row, field_names, datetime_parse_fmt=default_parse_fmt, has_durati
 
 
 @log_function_entry_and_exit
-def parse_csv_reader_content(csv_reader, work_days=default_work_days):
+def parse_csv_reader_content(input_data, **kwargs):
     # TODO: ADD DOCSTRING
-    def help_func(nested_obj, first_key, second_key, new_val):
+    def help_func(nested_obj, week, day, new_val):
         try:
-            nested_obj[first_key][second_key].append(new_val)
+            nested_obj[week][day].append(new_val)
         except KeyError as e:
-            if e.args[0] == first_key:
+            if e.args[0] == week:
                 # if it's the first time we visit this week
-                nested_obj[first_key] = dict()
-                nested_obj[first_key][second_key] = [new_val]
-            if e.args[0] == second_key:
+                nested_obj[week] = dict()
+                nested_obj[week][day] = [new_val]
+            if e.args[0] == day:
                 # If it's the first day we visit this day of this week
-                nested_obj[first_key][second_key] = [new_val]
+                nested_obj[week][day] = [new_val]
 
     total_sum = timedelta(0)
-    aggregate_records, overtime_records = dict(), dict()
-    for row in csv_reader:
-        (duration, (_, week_nr, week_day)) = parse_row(row)
-        total_sum += duration
+    aggregate_records = dict()
+    for row in input_data:
+        (duration, (_, week_nr, week_day)) = parse_row(row, **kwargs)
 
-        if week_day not in work_days.values():
-            help_func(
-                nested_obj=overtime_records,
-                first_key=week_nr,
-                second_key=week_day,
-                new_val=duration)
-        else:
-            help_func(
-                nested_obj=aggregate_records,
-                first_key=week_nr,
-                second_key=week_day,
-                new_val=duration)
+        t = datetime.strptime(duration, "%H:%M")
+        total_sum += timedelta(hours=t.hour, minutes=t.minute)
 
-    # add total sum to return object
-    aggregate_records["total_sum"] = total_sum
+        help_func(
+            nested_obj=aggregate_records,
+            week=week_nr,
+            day=week_day,
+            new_val=duration)
 
-    return aggregate_records, overtime_records
+    # add total amount of seconds to return object
+    aggregate_records["total_sum"] = int(total_sum.total_seconds())
+
+    return aggregate_records
 
 
 @log_function_entry_and_exit
 def parse_aggregate_weeks_and_weekdays(aggregate_data, hours_per_week=default_work_hours_in_week):
     # TODO: ADD DOCSTRING
-    total_balance = timedelta(0)
+
+    def get_timedelta_from_str(input_str, parse_fmt="%H:%M"):
+        t = datetime.strptime(input_str, parse_fmt)
+        return timedelta(hours=t.hour, minutes=t.minute)
+
+    total_balance, hours_per_week = timedelta(0), timedelta(hours=hours_per_week)
     for week, days in aggregate_data.items():
         if week == "total_sum":
             continue
+
         week_sum = timedelta(0)
         for day, records in days.items():
-            if day == "balance":
-                continue
             for record in records:
-                week_sum += record
-        week_balance = hours_per_week - week_sum
-        aggregate_data[week]["balance"] = week_balance
+                week_sum += get_timedelta_from_str(record)
+        week_balance = week_sum - hours_per_week
+
+        aggregate_data[week]["sum"] = int(week_sum.total_seconds())
+        aggregate_data[week]["balance"] = int(week_balance.total_seconds())
         total_balance += week_balance
 
-    total_balance = aggregate_data["total_sum"] - total_balance
+    total_balance = aggregate_data["total_sum"] - int(total_balance.total_seconds())
+    aggregate_data["hours_per_week"] = hours_per_week.total_seconds() / 3600
     aggregate_data["total_balance"] = total_balance
     return aggregate_data
--- tests/calculations_test.py
+++ tests/calculations_test.py
@@ -6,6 +6,8 @@ import pytest
 # Module import(s):
 from src.file_manip import get_csv_file_content_as_dicts
 from src.calculations import parse_row
+from src.calculations import parse_csv_reader_content
+from src.calculations import parse_aggregate_weeks_and_weekdays
 
 
 CSV_FILES_FOLDER = op.join("tests", "data")
@@ -43,3 +45,184 @@ def test_valid_parse_row(file_name, field_names, parse_fmt, has_duration, expect
     for x in rows:
         # Make sure all in rows are present in expected
         assert x in expected
+
+
+@pytest.mark.parametrize(
+    "input_data,expected,kwargs",
+    [
+        (
+            [
+                # input_data
+                {
+                    "Start time": "27-11-2016",
+                    "End time": "28-11-2016",
+                    "Hours": "0.00",
+                    "Income": "0.00",
+                    "First Check-In": "28-11-2016 08:20:00",
+                    "Last Check-Out": "28-11-2016 08:20:00",
+                },
+                {
+                    "Start time": "31-08-2016",
+                    "End time": "01-09-2016",
+                    "Hours": "7.00",
+                    "Income": "0.00",
+                    "First Check-In": "31-08-2016 10:00:00",
+                    "Last Check-Out": "31-08-2016 17:00:00",
+                },
+                {
+                    "Start time": "06-10-2016",
+                    "End time": "07-10-2016",
+                    "Hours": "6.67",
+                    "Income": "0.00",
+                    "First Check-In": "07-10-2016 08:45:00",
+                    "Last Check-Out": "07-10-2016 15:25:00",
+                },
+                {
+                    "Start time": "05-10-2016",
+                    "End time": "06-10-2016",
+                    "Hours": "8.08",
+                    "Income": "0.00",
+                    "First Check-In": "06-10-2016 08:15:00",
+                    "Last Check-Out": "06-10-2016 16:20:00",
+                },
+                {
+                    "Start time": "04-10-2016",
+                    "End time": "05-10-2016",
+                    "Hours": "7.50",
+                    "Income": "0.00",
+                    "First Check-In": "04-10-2016 09:10:00",
+                    "Last Check-Out": "04-10-2016 16:40:00",
+                }
+            ],
+            {
+                # expected:
+                48: {1: ['00:00']},
+                40: {2: ['07:30'], 4: ['08:05'], 5: ['06:40']},
+                35: {3: ['07:00']},
+                'total_sum': 105300,
+            },
+            {
+                # kwargs:
+                "field_names": ["First Check-In", "Last Check-Out"],
+                "datetime_parse_fmt": "%d-%m-%Y %H:%M:%S",
+                "has_duration": False,
+            }
+        ),
+        (
+            [
+                # input_data
+                {
+                    "Billable status": "Open / Not billable",
+                    "Date": "25.07.2016",
+                    "Day": "Mon",
+                    "From": "0.00",
+                    "To": "06:30",
+                    "Break": "",
+                    "Duration": "06:30",
+                    "Project": "meh",
+                    "Project no.": "lol",
+                    "Activity": "Egen kompetansebygging/kurs",
+                    "Comment": "Opplæring/introduksjon",
+                    "Reason for declining": "",
+                    "": "",
+                },
+            ],
+            {
+                # expected:
+                'total_sum': 23400,
+                30: {
+                    1: ['06:30'],
+                },
+            },
+            {
+                # kwargs:
+                "field_names": ["Date", "Duration"],
+                "datetime_parse_fmt": "%d.%m.%Y",
+                "has_duration": True,
+            }
+        ),
+    ])
+def test_valid_csv_reader_content(input_data, expected, kwargs):
+
+    return_data = parse_csv_reader_content(
+        input_data=input_data,
+        **kwargs)
+
+    assert return_data == expected
+
+
+@pytest.mark.parametrize(
+    "input_data,hours_per_week,expected",
+    [
+        (
+            {
+                # input_data
+                'total_sum': 23400,
+                30: {
+                    1: ['06:30'],
+                },
+            },
+            # {
+            #     # work_days:
+            #     "mon": 1, "tue": 2, "wed": 3, "thu": 4, "fri": 5,
+            # },
+            # hours_per_week
+            40,
+            {
+                # expected
+                'total_sum': 23400,
+                'total_balance': 144000,
+                'hours_per_week': 40.0,
+                30: {
+                    'balance': -120600,
+                    1: ['06:30'],
+                    'sum': 23400
+                },
+            },
+        ),
+        (
+            {
+                # input_data
+                48: {1: ['00:00']},
+                40: {2: ['07:30'], 4: ['08:05'], 5: ['06:40']},
+                35: {3: ['07:00']},
+                'total_sum': 18900,
+            },
+            # {
+            #     # work_days:
+            #     "mon": 1, "tue": 2, "wed": 3, "thu": 4, "fri": 5,
+            # },
+            # hours_per_week
+            40,
+            {
+                # expected
+                48: {
+                    1: ['00:00'],
+                    'sum': 0,
+                    'balance': -144000,
+                },
+                40: {
+                    'balance': -63900,
+                    2: ['07:30'],
+                    'sum': 80100,
+                    4: ['08:05'],
+                    5: ['06:40'],
+                },
+                35: {
+                    3: ['07:00'],
+                    'balance': -118800,
+                    'sum': 25200
+                },
+                'total_balance': 345600,
+                'total_sum': 18900,
+                'hours_per_week': 40.0,
+            },
+        ),
+    ])
+def test_valid_parse_aggregate_weeks_and_weekdays(input_data, hours_per_week, expected):
+
+    return_data = parse_aggregate_weeks_and_weekdays(
+        aggregate_data=input_data,
+        hours_per_week=hours_per_week, )
+
+    assert return_data == expected
