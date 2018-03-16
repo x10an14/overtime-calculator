7c45c97e6c53c7742f544a308fc7f6532f614edc
x10an14 <x10an14@gmail.com>
2018-03-16 22:13:53 +0100

    Bugfix: All tests run

--- overtime_calculator/src/auth.py
+++ overtime_calculator/src/auth.py
@@ -21,31 +21,29 @@ def get_user_folder(username: str) -> Path:
 
 
 @hug.post('/register')
-def register_user(username, password):
+def register_user(username: str, password: str):
     user_folder = get_user_folder(username)
-    if user_folder.exists():
+    user_pw_file = user_folder / 'password.txt'
+    if user_pw_file.exists():
         return {'error' : 'username already in use'}
-    try:
-        hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt()) # 12 is default salt rounds
-        user_pw_file = user_folder / 'password.txt'
-        with user_pw_file.open(mode='w') as f:
-            f.write(hashed_password)
-        return {'status' : 'ok'}
-    except:
-        return {'error' : 'something went wrong with user registration'}
+    # try:
+    hashed_password = bcrypt.hashpw(str.encode(password), bcrypt.gensalt()) # 12 is default salt rounds
+    with user_pw_file.open(mode='wb') as f:
+        f.write(hashed_password)
+    return {'status' : 'ok'}
+    # except:
+    #     return {'error' : 'something went wrong with user registration'}
 
 
 @hug.post('/signin')
-def signin_user(username: str, password: str) -> Dict[str, str]:
+def signin_user(username: str, password: str):
     secret = get_secret()
     user_folder = get_user_folder(username)
-    if user_folder.exists():
-        try:
-            user_pw_file = user_folder / 'password.txt'
-            with user_pw_file.open() as f:
-                hashed_password = f.readline()
-            if bcrypt.checkpw(str.encode(password), hashed_password):
-                return {"token" : jwt.encode({'user': username}, secret, algorithm='HS256')}
-        except:
-            return {'error' : 'something went wrong with user signin'}
-    return {'error': 'Invalid credentials'}
+    user_pw_file = user_folder / 'password.txt'
+    if not user_pw_file.exists():
+        return {'error': 'Invalid credentials'}
+
+    with user_pw_file.open(mode='rb') as f:
+        hashed_password = f.readline()
+    if bcrypt.checkpw(str.encode(password), hashed_password):
+        return {"token" : jwt.encode({'user': username}, secret, algorithm='HS256')}
